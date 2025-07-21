f4f3d0a74ddfc5e89830d1a0b45e24c23fad9636
Farid SEBOU <seboufarid43@gmail.com>
2025-07-21 11:31:42 +0000

    new

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* ----- NAVIGATION BAR FUNCTION ----- */
+/* ----- NAVIGATION BAR FUNCTION ----- 
 function myMenuFunction(){
     var menuBtn = document.getElementById("myNavMenu");
 
@@ -8,8 +8,8 @@ function myMenuFunction(){
       menuBtn.className = "nav-menu";
     }
   }
-
-/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+*/
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- 
   window.onscroll = function() {headerShadow()};
 
   function headerShadow() {
@@ -29,7 +29,7 @@ function myMenuFunction(){
 
     }
   }
-
+*/
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
@@ -41,15 +41,15 @@ function myMenuFunction(){
  })
 
 
-/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- 
  const sr = ScrollReveal({
         origin: 'top',
         distance: '80px',
         duration: 2000,
         reset: true     
  })
-
-/* -- HOME -- */
+*/
+/* -- HOME -- 
 sr.reveal('.featured-text-card',{})
 sr.reveal('.featured-name',{delay: 100})
 sr.reveal('.featured-text-info',{delay: 200})
@@ -57,16 +57,16 @@ sr.reveal('.featured-text-btn',{delay: 200})
 sr.reveal('.social_icons',{delay: 200})
 sr.reveal('.featured-image',{delay: 300})
 
-
+*/
 /* -- PROJECT BOX -- */
-sr.reveal('.project-box',{interval: 200})
+// sr.reveal('.project-box',{interval: 200})
 
 /* -- HEADINGS -- */
-sr.reveal('.top-header',{})
+// sr.reveal('.top-header',{})
 
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
 
-/* -- ABOUT INFO & CONTACT INFO -- */
+/* -- ABOUT INFO & CONTACT INFO -- 
 const srLeft = ScrollReveal({
   origin: 'left',
   distance: '80px',
@@ -76,8 +76,8 @@ const srLeft = ScrollReveal({
 
 srLeft.reveal('.about-info',{delay: 100})
 srLeft.reveal('.contact-info',{delay: 100})
-
-/* -- ABOUT SKILLS & FORM BOX -- */
+*/
+/* -- ABOUT SKILLS & FORM BOX -- 
 const srRight = ScrollReveal({
   origin: 'right',
   distance: '80px',
@@ -88,9 +88,9 @@ const srRight = ScrollReveal({
 srRight.reveal('.skills-box',{delay: 100})
 srRight.reveal('.form-control',{delay: 100})
 
+*/
 
-
-/* ----- CHANGE ACTIVE LINK ----- */
+/* ----- CHANGE ACTIVE LINK ----- 
 
 const sections = document.querySelectorAll('section[id]')
 
@@ -115,3 +115,4 @@ function scrollActive() {
 }
 
 window.addEventListener('scroll', scrollActive)
+*/
